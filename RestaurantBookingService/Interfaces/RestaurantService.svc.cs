11fa2f530c2f545930dcadc69797b165add01ec5
Virmantas Cekys <1067604@ucn.dk>
2018-11-07 12:07:30 +0100

    Restaurant service interface. Database library

---
+++ RestaurantBookingService/Interfaces/RestaurantService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace RestaurantBookingService.Interfaces
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "RestaurantService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select RestaurantService.svc or RestaurantService.svc.cs at the Solution Explorer and start debugging.
+    public class RestaurantService : IRestaurantService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
