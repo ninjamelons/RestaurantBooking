ec4fe1282c86b23e5e3ba9c1a1962e07ae31cccb
Samuel Chudý <samo.chudy.869@gmail.com>
2018-11-08 09:55:42 +0100

    VerifyRestaurantTest

---
+++ UnitTestProject1/VerifyRestaurantTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    class VerifyRestaurantTest
+    {
+        [TestMethod]
+        [DataRow("RestaurantName", "Good Address", "+45 50 38 97 04", "ohshit@gmail.com", true, DisplayName = "Restaurant is verify!")]
+        [DataRow("Restadfds", "Good Address", "+45 50 38 97 04", "ohshit@gmail.com", false, DisplayName = "Restaurant is not verify!")]
+        public void Test_Restaurant_Verify(string name, string address, string phoneNo, string email, bool shouldValidate, bool verify)
+
+    }
+}
