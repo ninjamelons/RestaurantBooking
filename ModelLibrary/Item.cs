915a7bd9cc6f513c6478e33363803aad62c4df36
GolvinM <simonjensen657@hotmail.com>
2018-11-08 11:59:18 +0100

    Order and Item model and OrderTest

---
+++ ModelLibrary/Item.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelLibrary
+{
+    public class Item
+    {
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Invalid number")]
+        public string Id { get; set; }
+
+        [RegularExpression("^[0-9A-Za-z ]{1,30}$", ErrorMessage = "Invalid name")]
+        public string Name { get; set; }
+
+        [RegularExpression("^[0-9A-Za-z ]{1,500}$", ErrorMessage = "Invalid description")]
+        public string Description { get; set; }
+
+        [RegularExpression("^[0-9A-Za-z ]{1,30}$", ErrorMessage = "Invalid category name")]
+        public string Category { get; set; }
+    }
+}
