720c4e1b22d2fcaeba1ac4b6dfe2272d9ebfc33d
Filip Olsen <1067716@ucn.dk>
2018-11-21 08:53:15 +0100

    Order stuff

--- RestaurantService/RestaurantService.cs
+++ RestaurantService/RestaurantService.cs
@@ -15,12 +15,28 @@ namespace RestaurantService
     {
         public void CreateTable(Table table)
         {
-            throw new NotImplementedException();
+            TableCtrl tableCtrl = new TableCtrl();
+            tableCtrl.CreateTable(table);
         }
-
+        public IEnumerable<Table> GetAllTables(int restaurantId)
+        {
+            TableCtrl tableCtrl = new TableCtrl();
+            return tableCtrl.GetRestaurantTables(restaurantId);
+        }
+        public Table GetTable(Table table)
+        {
+            TableCtrl tableCtrl = new TableCtrl();
+            return tableCtrl.GetTable(table);
+        }
+        public void UpdateTable(Table oldTable, Table newTable)
+        {
+            TableCtrl tableCtrl = new TableCtrl();
+            tableCtrl.UpdateTable(oldTable, newTable);
+        }
         public void DeleteTable(Table table)
         {
-            throw new NotImplementedException();
+            TableCtrl tableCtrl = new TableCtrl();
+            tableCtrl.DeleteTable(table);
         }
 
         public IEnumerable<ModelLibrary.Restaurant> GetAllRestaurants()
@@ -59,26 +75,11 @@ namespace RestaurantService
             return mRes;
         }
 
-        public IEnumerable<Table> GetAllTables(ModelLibrary.Restaurant restaurant)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<Table> GetTable(Table table)
-        {
-            throw new NotImplementedException();
-        }
-
         public void CreateRestaurant(ModelLibrary.Restaurant restaurant)
         {
             JustFeastDbDataContext db = new JustFeastDbDataContext();
             db.Restaurants.InsertOnSubmit(RestaurantCtrl.ConvertRestaurantToDatabase(restaurant));
             db.SubmitChanges();
-        }
-
-        public void UpdateTable(Table table)
-        {
-            throw new NotImplementedException();
         }
 
         public void UpdateRestaurant(ModelLibrary.Restaurant restaurant)
@@ -114,21 +115,6 @@ namespace RestaurantService
         }
 
         public ModelLibrary.Restaurant GetRestaurant(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<Table> GetAllTables(int restaurantId)
-        {
-            throw new NotImplementedException();
-        }
-
-        Table IRestaurantService.GetTable(Table table)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void UpdateTable(Table oldTable, Table newTable)
         {
             throw new NotImplementedException();
         }
