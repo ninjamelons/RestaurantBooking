1248660c872eea01b227a3131478c753649416ee
GolvinM <simonjensen657@hotmail.com>
2018-11-23 13:53:23 +0100

    Order stuff (including adding items to an order)

---
+++ RestaurantService/IOrderService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using ModelLibrary;
+
+namespace RestaurantService
+{
+    public interface IOrderService
+    {
+        ModelLibrary.Order GetOrderById(int id);
+
+        void CreateOrder(Order order);
+
+        void UpdateOrder(Order order);
+
+        void AddItemToOrder(int orderId, int itemId);
+    }
+}
