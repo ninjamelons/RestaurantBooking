132275e42fbb2f7adee3005e019ba8cd31cb92ca
GolvinM <simonjensen657@hotmail.com>
2018-11-23 14:22:21 +0100

    OrderService

--- RestaurantService/IOrderService.cs
+++ RestaurantService/IOrderService.cs
@@ -4,14 +4,19 @@ using ModelLibrary;
 
 namespace RestaurantService
 {
+    [ServiceContract]
     public interface IOrderService
     {
+        [OperationContract]
         ModelLibrary.Order GetOrderById(int id);
 
+        [OperationContract]
         void CreateOrder(Order order);
 
+        [OperationContract]
         void UpdateOrder(Order order);
 
+        [OperationContract]
         void AddItemToOrder(int orderId, int itemId);
     }
 }
