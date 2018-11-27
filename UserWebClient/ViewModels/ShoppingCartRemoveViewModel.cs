8a0c594a6057594b7391a030bc48482b2bf66a10
Samuel Chudý <samo.chudy.869@gmail.com>
2018-11-27 09:44:39 +0100

    shhd

---
+++ UserWebClient/ViewModels/ShoppingCartRemoveViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UserWebClient.Models;
+using ModelLibrary;
+using DatabaseAccessLibrary;
+
+namespace UserWebClient.ViewModels
+{
+    public class ShoppingCartRemoveViewModel
+    {
+        public string Message { get; set; }
+        public decimal CartTotal { get; set; }
+        public int CartCount { get; set; }
+        public int ItemCount { get; set; }
+        public int DeleteId { get; set; }
+    }
+}
