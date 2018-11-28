d8b546b8a22bc2e2ad6282937cfb17ad6c099eaf
Virmantas Cekys <1067604@ucn.dk>
2018-11-28 08:59:29 +0100

    Home Page fix

--- UserWebClient/Controllers/ShoppingCartController.cs
+++ UserWebClient/Controllers/ShoppingCartController.cs
@@ -5,8 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using ModelLibrary;
 using DatabaseAccessLibrary;
-using UserWebClient.Views;
-using UserWebClient.Views.ViewModels;
+//using UserWebClient.Views;
+//using UserWebClient.Views.ViewModels;
 /*
 namespace UserWebClient.Controllers
 {
---
+++ UserWebClient/Models/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UserWebClient.Models
+{
+    public class HomeViewModel
+    {
+        public int Zipcode { get; set; }
+    }
+}
