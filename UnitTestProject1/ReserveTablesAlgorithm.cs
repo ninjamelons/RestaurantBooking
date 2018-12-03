0a6943ebfc9fb55e0de9b1fe552c3324ee9fe2f5
GolvinM <simonjensen657@hotmail.com>
2018-12-03 13:33:50 +0100

    Table algorithm

--- UnitTestProject1/ReserveTablesAlgorithm.cs
+++ UnitTestProject1/ReserveTablesAlgorithm.cs
@@ -57,6 +57,37 @@ namespace UnitTestProject1
             //Assert
             Assert.AreEqual(20, moduloDict.Count);
         }
+        
+        [TestMethod]
+        [DataRow(9)]
+        [DataRow(10)]
+        [DataRow(12)]
+        [DataRow(14)]
+        [DataRow(16)]
+        public void Least_Number_Of_Tables_Per_Order_Test(int tempSeats)
+        {
+            var tableC = new TableCtrl();
+
+            #region tablesList
+
+            var id = 1000000;
+            var tables = new List<Table>();
+            tables.Add(new Table { NoSeats = 2, Reserved = false, RestaurantId = id });
+            tables.Add(new Table { NoSeats = 2, Reserved = false, RestaurantId = id });
+            tables.Add(new Table { NoSeats = 4, Reserved = false, RestaurantId = id });
+            tables.Add(new Table { NoSeats = 6, Reserved = false, RestaurantId = id });
+            tables.Add(new Table { NoSeats = 8, Reserved = false, RestaurantId = id });
+            tables.Add(new Table { NoSeats = 10, Reserved = false, RestaurantId = id });
+
+            #endregion
+
+            //Act
+            var tablesUsed = tableC.LeastNumberOfTables(tables, tempSeats);
+
+            //Assert
+            Assert.IsTrue(3 > tablesUsed.Count);
+
+        }
 
         [TestMethod]
         public void FindSmallestModuloCount()
