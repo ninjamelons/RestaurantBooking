3748f77a4dd1ae2677c63190f626807e4aa0413d
Virmantas Cekys <1067604@ucn.dk>
2018-12-03 11:07:10 +0100

    Desktop Login

---
+++ RestaurantDesktopClient/PasswordEncrypt.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantDesktopClient
+{
+    public static class PasswordEncrypt
+    {
+        public static string HashPasswordString(string email, string password)
+        {
+            return GetHashString(password + email);
+        }
+        private static byte[] GetHash(string inputString)
+        {
+            HashAlgorithm algorithm = MD5.Create();
+            return algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString));
+        }
+
+        private static string GetHashString(string inputString)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in GetHash(inputString))
+                sb.Append(b.ToString("X2"));
+
+            return sb.ToString();
+        }
+    }
+}
