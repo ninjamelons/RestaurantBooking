fd8ce4c8c803b48e80a068f2004d80b9b2469bb6
Samuel Chudý <samo.chudy.869@gmail.com>
2018-12-04 11:33:10 +0100

    nn

--- UserWebClient/Controllers/ShoppingCartController.cs
+++ UserWebClient/Controllers/ShoppingCartController.cs
@@ -27,7 +27,7 @@ namespace UserWebClient.Controllers
 
             if (Session["orderId"] != null)
             {
-                //order = _orderProxy.GetOrderById((int)Session["orderId"]);
+               // order = _orderProxy.GetOrderById((int)Session["orderId"]);
             }
 
             return View("HomeCart", order);
@@ -47,7 +47,7 @@ namespace UserWebClient.Controllers
             ModelLibrary.Order order = null;
             if (Session["orderId"] != null)
             {
-               // order = _orderProxy.GetOrderById((int)Session["orderId"]);
+                //order = _orderProxy.GetOrderById((int)Session["orderId"]);
             }
 
             return View("HomeCart", order);
