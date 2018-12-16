1faf040ee3206a1f3d710f2962020a43d2f55532
GolvinM <simonjensen657@hotmail.com>
2018-12-16 11:54:05 +0100

    Db

--- DatabaseAccessLibrary/Properties/Settings.Designer.cs
+++ DatabaseAccessLibrary/Properties/Settings.Designer.cs
@@ -76,5 +76,16 @@ namespace DatabaseAccessLibrary.Properties {
                 return ((string)(this["JustFeastDbConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=localhost;Initial Catalog=JustFeast;Persist Security Info=True;User I" +
+            "D=sa;Password=dmai0917")]
+        public string JustFeastConnectionString1 {
+            get {
+                return ((string)(this["JustFeastConnectionString1"]));
+            }
+        }
     }
 }
